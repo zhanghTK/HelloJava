8cb5ba90634b19607d399f069d8a440646abe9c6
ZhangHao <510223064@qq.com>
2016-04-22 19:13:35 +0800

    add game util classes

---
+++ src/main/java/tk/zhangh/java/game/solar/util/Constant.java
@@ -0,0 +1,16 @@
+package tk.zhangh.java.game.solar.util;
+
+/**
+ * 窗口使用常量类
+ * Created by ZhangHao on 2016/4/22.
+ */
+public class Constant {
+    /**
+     * 窗口宽
+     */
+    public static final int WINDOW_WIDTH = 800;
+    /**
+     * 窗口高
+     */
+    public static final int WINDOW_HEIGHT = 600;
+}
---
+++ src/main/java/tk/zhangh/java/game/solar/util/MyFrame.java
@@ -0,0 +1,45 @@
+package tk.zhangh.java.game.solar.util;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * 生成窗口基础类
+ * Created by ZhangHao on 2016/4/22.
+ */
+public class MyFrame extends Frame {
+
+    /**
+     * 加载窗口
+     */
+    public void lunchFrame() {
+        setSize(Constant.WINDOW_WIDTH, Constant.WINDOW_HEIGHT);
+        setLocation(0, 0);
+        setVisible(true);
+        new PaintThread().start();
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    /**
+     * 重画窗口
+     */
+    class PaintThread extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                repaint();
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}

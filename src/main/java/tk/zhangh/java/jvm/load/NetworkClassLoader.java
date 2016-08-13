544853678a3227bdf6af5f1d4d28fadfdfaea0d0
ZhangHao <510223064@qq.com>
2016-08-13 11:05:04 +0800

    add NetworkClassLoader

---
+++ src/main/java/tk/zhangh/java/jvm/load/NetworkClassLoader.java
@@ -0,0 +1,46 @@
+package tk.zhangh.java.jvm.load;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * 网络类加载器
+ * Created by ZhangHao on 2016/8/12.
+ */
+public class NetworkClassLoader extends ClassLoader{
+    private String rootUrl;
+
+    public NetworkClassLoader(String rootUrl) {
+        this.rootUrl = rootUrl;
+    }
+
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        byte[] classData = getClassData(name);
+        if (classData == null) {
+            throw new ClassNotFoundException();
+        }
+        else {
+            return defineClass(name, classData, 0, classData.length);
+        }
+    }
+
+    private byte[] getClassData(String className) {
+        String path = rootUrl + "/" + className.replace('.', '/') + ".class";
+        try {
+            URL url = new URL(path);
+            InputStream ins = url.openStream();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int bufferSize = 4096;
+            byte[] buffer = new byte[bufferSize];
+            int bytesNumRead;
+            while ((bytesNumRead = ins.read(buffer)) != -1) {
+                baos.write(buffer, 0, bytesNumRead);
+            }
+            return baos.toByteArray();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}

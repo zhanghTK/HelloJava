3dbd914110e435d4cd62a79984b66e62428fbc26
ZhangHao <510223064@qq.com>
2016-08-13 11:59:47 +0800

    add ReverseClassLoader and FileEncryptUtil(reverse content)

---
+++ src/main/java/tk/zhangh/java/jvm/load/ReverseClassLoader.java
@@ -0,0 +1,43 @@
+package tk.zhangh.java.jvm.load;
+
+import tk.zhangh.java.io.InputStreamProcessingTemplate;
+import tk.zhangh.java.io.InputStreamProcessor;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by ZhangHao on 2016/8/13.
+ */
+public class ReverseClassLoader extends ClassLoader{
+    private String rootDir;
+
+    public ReverseClassLoader(String rootDir) {
+        this.rootDir = rootDir;
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        Class clazz;
+        InputStreamProcessor processor = new InputStreamProcessor() {
+            @Override
+            public Object process(InputStream inStream) throws IOException {
+                ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+                int tmp;
+                while ((tmp = inStream.read()) != -1) {
+                    outStream.write(tmp ^ 0xff);
+                }
+                return outStream.toByteArray();
+            }
+        };
+        String path = rootDir + "/" + name.replace(".", "/") + ".class";
+        byte[] classData = (byte[]) InputStreamProcessingTemplate.process(path, processor);
+        if (classData == null) {
+            throw new ClassNotFoundException();
+        }else {
+            clazz =defineClass(name, classData, 0, classData.length);
+        }
+        return clazz;
+    }
+}
---
+++ src/test/java/tk/zhangh/java/io/FileEncryptUtilTest.java
@@ -0,0 +1,15 @@
+package tk.zhangh.java.io;
+
+import org.junit.Test;
+import tk.zhangh.java.jvm.load.ReverseClassLoaderTest;
+
+/**
+ * Created by ZhangHao on 2016/8/13.
+ */
+public class FileEncryptUtilTest {
+
+    @Test
+    public void testReverse() throws Exception {
+        new ReverseClassLoaderTest().testFindClass();
+    }
+}

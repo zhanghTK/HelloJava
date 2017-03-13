3389fa09d4da4d53aaaa44d73385bb6543119e56
ZhangHao <510223064@qq.com>
2017-03-13 20:55:39 +0800

    feat add server exception
    add server exception

---
+++ x/src/main/java/tk/zhangh/java/x/server/exception/RequestInitException.java
@@ -0,0 +1,7 @@
+package tk.zhangh.java.x.server.exception;
+
+/**
+ * Created by ZhangHao on 2017/3/13.
+ */
+public class RequestInitException extends RuntimeException {
+}
---
+++ x/src/main/java/tk/zhangh/java/x/server/exception/ServerInitException.java
@@ -0,0 +1,10 @@
+package tk.zhangh.java.x.server.exception;
+
+/**
+ * Created by ZhangHao on 2017/3/13.
+ */
+public class ServerInitException extends RuntimeException {
+    public ServerInitException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
